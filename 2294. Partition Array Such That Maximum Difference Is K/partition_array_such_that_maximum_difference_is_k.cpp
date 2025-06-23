2ca645aa091cba2818532062c9d62654311fa651
Gitika Goyal <gitikagoyal1115@gmail.com>
2025-06-23 21:16:17 +0530

    Create partition_array_such_that_maximum_difference_is_k.cpp

---
+++ 2294. Partition Array Such That Maximum Difference Is K/partition_array_such_that_maximum_difference_is_k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        //result will get stored in subsequences
+        int subsequences = 0;
+        int min_val = nums[0];
+        for(int i=1;i<n;++i){
+            if(nums[i]-min_val > k){
+                min_val = nums[i];
+                subsequences++;
+            }
+        }
+        return subsequences+1;  //last one didnt get count
+    }
+};
