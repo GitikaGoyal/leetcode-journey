00f942c15df2ed2df38c0ec58bb39e15af442243
Gitika Goyal <gitikagoyal1115@gmail.com>
2025-07-11 12:06:27 +0530

    Create meeting_Rooms_II.py

---
+++ 253. Meeting Rooms II/meeting_Rooms_II.py
@@ -0,0 +1,60 @@
+# Min-Heap
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key=lambda x: x.start)
+        min_heap = []
+
+        for interval in intervals:
+            if min_heap and min_heap[0] <= interval.start:
+                heapq.heappop(min_heap)
+            heapq.heappush(min_heap, interval.end)
+
+        return len(min_heap)
+
+#Sweep Line Alogorithm
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        mp = defaultdict(int)
+        for i in intervals:
+            mp[i.start] += 1
+            mp[i.end] -= 1
+        prev = 0
+        res = 0
+        for i in sorted(mp.keys()):
+            prev += mp[i]
+            res = max(res, prev)
+        return res
+
+#Two-Pointers
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+        
+        res = count = 0
+        s = e = 0
+        while s < len(intervals):
+            if start[s] < end[e]:
+                s += 1
+                count += 1
+            else:
+                e += 1
+                count -= 1
+            res = max(res, count)
+        return res
+
+#Greedy
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        time = []
+        for i in intervals:
+            time.append((i.start, 1))
+            time.append((i.end, -1))
+        
+        time.sort(key=lambda x: (x[0], x[1]))
+        
+        res = count = 0
+        for t in time:
+            count += t[1]
+            res = max(res, count)
+        return res
