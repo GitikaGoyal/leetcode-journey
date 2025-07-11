4e01600ae328d0b0126c77546b69ff0b6d704051
Gitika Goyal <gitikagoyal1115@gmail.com>
2025-07-11 12:03:41 +0530

    Create meeting_rooms_II.cpp

---
+++ 253. Meeting Rooms II/meeting_rooms_II.cpp
@@ -0,0 +1,87 @@
+// Min-Heap
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
+            return a.start < b.start;
+        });
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for (const auto& interval : intervals) {
+            if (!minHeap.empty() && minHeap.top() <= interval.start) {
+                minHeap.pop();
+            }
+            minHeap.push(interval.end);
+        }
+        return minHeap.size();
+    }
+};
+
+//Sweep Line Alogorithm
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
+            return a.start < b.start;
+        });
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for (const auto& interval : intervals) {
+            if (!minHeap.empty() && minHeap.top() <= interval.start) {
+                minHeap.pop();
+            }
+            minHeap.push(interval.end);
+        }
+        return minHeap.size();
+    }
+};
+
+//Two-Pointers
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        vector<int> start, end;
+        
+        for (const auto& i : intervals) {
+            start.push_back(i.start);
+            end.push_back(i.end);
+        }
+        
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+        
+        int res = 0, count = 0, s = 0, e = 0;
+        while (s < intervals.size()) {
+            if (start[s] < end[e]) {
+                s++;
+                count++;
+            } else {
+                e++;
+                count--;
+            }
+            res = max(res, count);
+        }
+        return res;
+    }
+};
+
+// Greedy
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        vector<pair<int, int>> time;
+        for (const auto& i : intervals) {
+            time.push_back({i.start, 1});
+            time.push_back({i.end, -1});
+        }
+        
+        sort(time.begin(), time.end(), [](auto& a, auto& b) {
+            return a.first == b.first ? a.second < b.second : a.first < b.first;
+        });
+        
+        int res = 0, count = 0;
+        for (const auto& t : time) {
+            count += t.second;
+            res = max(res, count);
+        }
+        return res;
+    }
+};
