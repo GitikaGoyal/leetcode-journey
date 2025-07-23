bbe30353554fcb9e05c18ed4ae4ebe3674e393d8
Gitika Goyal <gitikagoyal1115@gmail.com>
2025-07-23 21:38:42 +0530

    Create 184. Department Highest Salary.sql

---
+++ MySQL/184. Department Highest Salary.sql
@@ -0,0 +1,10 @@
+SELECT Department.name as Department, Employee.name as Employee, Employee.salary as Salary 
+FROM Employee JOIN Department 
+ON Employee.departmentid = Department.id 
+WHERE (Employee.departmentId, Employee.salary) IN (
+  SELECT 
+    departmentId, 
+    MAX(salary)
+  FROM Employee 
+  GROUP BY departmentId
+  );
